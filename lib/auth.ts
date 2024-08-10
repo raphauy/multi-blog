e915bd94cd9d4c99b03a564f107e7452ab479995
raphauy <rapha.uy@rapha.uy>
2024-08-10 11:32:22 -0300

    auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -11,7 +11,6 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: process.env.AUTH_GITHUB_ID as string,
       clientSecret: process.env.AUTH_GITHUB_SECRET as string,
-      accessTokenUrl: process.env.AUTH_GITHUB_ACCESS_TOKEN_URL as string,
       profile(profile) {
         return {
           id: profile.id.toString(),
